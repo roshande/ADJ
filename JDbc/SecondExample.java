8590f544c39dcff24d6552c2c1092977b2546656
student <student@www.pramod_demo.com>
2017-10-30 12:15:41 +0530

    JDBC

---
+++ JDbc/SecondExample.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDbc;
+
+/**
+ *
+ * @author Vikas Chauhan
+ */
+//STEP 1. Import required packages
+import java.sql.*;
+
+public class SecondExample {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/emp";
+
+//  Database credentials
+    static final String USER = "root";
+    static final String PASS = "";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        System.out.println("in main");
+        try {
+//STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+//STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+//STEP 4: Execute a query
+            System.out.println("Creating statement...");
+
+            //stmt = conn.prepareStatement("insert into employees(first,lat,age) values(?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+            stmt = conn.prepareStatement("insert into employees(first,last,age) values(?,?,?)");
+//id is auto-increment
+            //int noRec = 0;
+            //  while(noRec<2){ //to insert multiple records
+            stmt.setString(1, "abc");//1 specifies the first parameter in the query  
+            stmt.setString(2, "Ratan");
+            stmt.setInt(3, 20);
+
+            int i = stmt.executeUpdate();
+            System.out.println(i + " records inserted");
+//            noRec++;
+//            }
+            /*//////////////delete
+            stmt = conn.prepareStatement("delete from employees where first=?");
+            stmt.setString(1, "abc");
+
+            i = stmt.executeUpdate();
+            System.out.println(i + " records deleted");
+                    */
+            
+//STEP 6: Clean-up environment
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }//end main
+}//end FirstExample
